518bed0722376429d5b7777aef7c9fd16e81feb8
wilsonDT <detorres.wilson21@gmail.com>
2024-02-15 00:20:10 +0800

    added yehtoh.py

---
+++ yehtoh.py
@@ -0,0 +1,35 @@
+print("yehtoh!")
+
+
+# Create a function for fibonacci sequence
+def fibonacci(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+    print()
+
+# create a compatible function for the fibonacci sequence
+def fibonacci2(n):
+    a = 0
+    b = 1
+    result = []
+    for i in range(n):
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+# Create the algorithm for the fibonacci sequence
+def fibonacci3(n):
+    a = 0
+    b = 1
+    result = []
+    for i in range(n):
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+# print yehtoh 1000 times
+for i in range(1000):
+    print("yehtoh")
